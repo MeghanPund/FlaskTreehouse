10e9c9dfcc6fc544b7e1346a95ca13b276b350bd
Meghan Pund <meghan.pund@gmail.com>
2022-01-26 13:25:20 -0500

    query strings

--- simple_app.py
+++ simple_app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask import request # global
 
 app = Flask(__name__)
 
 @app.route('/') # decorator makes the function a view/controller
-def index():
-    return "Hello from Treehouse"
+def index(name="Treehouse"):
+    name = request.args.get('name', name)
+    # to change the value from Treehouse to a name, open the page
+    # append '?name='whatevername' to URL
+    return f"Hello from {name}"
 
 app.run(debug=True, port=8000, host='0.0.0.0') 
 # anytime we make changes, Flask will autorestart if debug=True
