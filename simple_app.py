15a0ae77341aa9f597cab9f83c2263aa7a93db89
Meghan Pund <meghan.pund@gmail.com>
2022-01-26 14:15:29 -0500

    context and render_template

--- simple_app.py
+++ simple_app.py
@@ -1,14 +1,20 @@
 from flask import Flask
-from flask import request # global
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/') # decorator makes the function a view/controller
+@app.route('/<name>') # captures whatever follows / as the name variable/arg
 def index(name="Treehouse"):
-    name = request.args.get('name', name)
-    # to change the value from Treehouse to a name, open the page
-    # append '?name='whatevername' to URL
     return f"Hello from {name}"
+
+@app.route('/<int:num1>/<float:num2>')
+@app.route('/<float:num1>/<int:num2>')
+@app.route('/<int:num1>/<int:num2>')
+@app.route('/<float:num1>/<float:num2>')
+def sum(num1, num2):
+    context = {'num1': num1, 'num2': num2}
+    return render_template("add.html", **context)
 
 app.run(debug=True, port=8000, host='0.0.0.0') 
 # anytime we make changes, Flask will autorestart if debug=True
